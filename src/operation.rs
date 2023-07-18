20c4d149eadcaa91a8fbb4187ce04742e13a4b90
Martin Hoffmann <martin@nlnetlabs.nl>
2023-07-18 09:56:45 +0200

    Update to latest domain.

--- src/operation.rs
+++ src/operation.rs
@@ -4,11 +4,9 @@ use std::fs::File;
 use std::{io};
 use std::future::Future;
 use std::str::FromStr;
-use bytes::Bytes;
-use domain::base::{Message, MessageBuilder, StreamTarget, ToDname};
+use domain::base::{Message, MessageBuilder, StreamTarget};
 use domain::base::iana::Class;
-use domain::base::Dname;
-use domain::rdata::{ZoneRecordData, Cname, Mb, Md, Mf, Minfo, Mr, Mx, Ns, Ptr, Soa, Srv, Rrsig, Nsec};
+use domain::base::name::{Dname, FlattenInto};
 use domain::zonefile::inplace::{Zonefile, Entry};
 //use domain::base::RecordData;
 use futures::future::{pending, Pending};
@@ -21,7 +19,7 @@ use crate::error::ExitError;
 use crate::net::server::{BufSource, DgramServer, StreamServer, Transaction};
 use crate::process::Process;
 use crate::store::Store;
-use crate::zones::{Answer, StoredDname, SharedZoneSet, StoredRecord, /*Zone*/};
+use crate::zones::{Answer, SharedZoneSet};
 
 pub fn prepare() -> Result<(), ExitError> {
     Process::init()?;
@@ -36,44 +34,13 @@ pub fn run(config: Config) -> Result<(), ExitError> {
     Runtime::new().map_err(|_| ExitError::Generic)?.block_on(async move {
         let reader = Zonefile::load(&mut File::open("/etc/nsd/example.com.zone").unwrap()).unwrap();
         let mut zonefile = crate::zonefile::Zonefile::new(
-            StoredDname::from_str("example.com.").unwrap(),
+            Dname::from_str("example.com.").unwrap(),
             Class::In
         );
         for item in reader {
             match item.unwrap() {
                 Entry::Record(record) => {
-                    let (owner, class, ttl, data) = (record.owner(), record.class(), record.ttl(), record.data());
-                    let owner: Dname<Bytes> = owner.to_dname().unwrap();
-                    let data: ZoneRecordData<Bytes, StoredDname> = match data.to_owned() {
-                        ZoneRecordData::A(v) => v.into(),
-                        ZoneRecordData::Cname(v) => ZoneRecordData::<Bytes, StoredDname>::Cname(Cname::new(v.to_dname().unwrap())),
-                        ZoneRecordData::Hinfo(v) => v.into(),
-                        ZoneRecordData::Mb(v) => ZoneRecordData::<Bytes, StoredDname>::Mb(Mb::new(v.madname().to_dname().unwrap())),
-                        ZoneRecordData::Md(v) => ZoneRecordData::<Bytes, StoredDname>::Md(Md::new(v.madname().to_dname().unwrap())),
-                        ZoneRecordData::Mf(v) => ZoneRecordData::<Bytes, StoredDname>::Mf(Mf::new(v.madname().to_dname().unwrap())),
-                        ZoneRecordData::Minfo(v) => ZoneRecordData::<Bytes, StoredDname>::Minfo(Minfo::new(v.rmailbx().to_dname().unwrap(), v.emailbx().to_dname().unwrap())),
-                        ZoneRecordData::Mr(v) => ZoneRecordData::<Bytes, StoredDname>::Mr(Mr::new(v.newname().to_dname().unwrap())),
-                        ZoneRecordData::Mx(v) => ZoneRecordData::<Bytes, StoredDname>::Mx(Mx::new(v.preference(), v.exchange().to_dname().unwrap())),
-                        ZoneRecordData::Ns(v) => ZoneRecordData::<Bytes, StoredDname>::Ns(Ns::new(v.nsdname().to_dname().unwrap())),
-                        ZoneRecordData::Ptr(v) => ZoneRecordData::<Bytes, StoredDname>::Ptr(Ptr::new(v.ptrdname().to_dname().unwrap())),
-                        ZoneRecordData::Soa(v) => ZoneRecordData::<Bytes, StoredDname>::Soa(Soa::new(v.mname().to_dname().unwrap(), v.rname().to_dname().unwrap(), v.serial(), v.refresh(), v.retry(), v.expire(), v.minimum())),
-                        ZoneRecordData::Txt(v) => v.into(),
-                        ZoneRecordData::Srv(v) => ZoneRecordData::<Bytes, StoredDname>::Srv(Srv::new(v.priority(), v.weight(), v.port(), v.target().to_dname().unwrap())),
-                        ZoneRecordData::Aaaa(v) => v.into(),
-                        ZoneRecordData::Dnskey(v) => v.into(),
-                        ZoneRecordData::Rrsig(v) => ZoneRecordData::<Bytes, StoredDname>::Rrsig(Rrsig::new(v.type_covered(), v.algorithm(), v.labels(), v.original_ttl(), v.expiration(), v.inception(), v.key_tag(), v.signer_name().to_dname().unwrap(), v.signature().clone())),
-                        ZoneRecordData::Nsec(v) => ZoneRecordData::<Bytes, StoredDname>::Nsec(Nsec::new(v.next_name().to_dname().unwrap(), v.types().clone())),
-                        ZoneRecordData::Ds(v) => v.into(),
-                        ZoneRecordData::Dname(v) => ZoneRecordData::<Bytes, StoredDname>::Dname(domain::rdata::Dname::new(v.to_dname().unwrap())),
-                        ZoneRecordData::Nsec3(v) => v.into(),
-                        ZoneRecordData::Nsec3param(v) => v.into(),
-                        ZoneRecordData::Cdnskey(v) => v.into(),
-                        ZoneRecordData::Cds(v) => v.into(),
-                        ZoneRecordData::Unknown(v) => v.into(),
-                        _ => todo!(),
-                    };
-                    let record = StoredRecord::new(owner, class, ttl, data);
-                    zonefile.insert(record).unwrap();
+                    zonefile.insert(record.flatten_into()).unwrap();
                 }
                 _ => panic!("unsupported item")
             }
