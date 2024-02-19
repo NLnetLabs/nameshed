5e1e0998f806bdae5f8b80efa6b30aa2beff6c08
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-02-19 15:08:03 +0100

    Dump domain, and minimize changes compared to the main branch.

--- src/operation.rs
+++ src/operation.rs
@@ -1,30 +1,28 @@
 //! Running the daemon.
 
+
+use std::fs::File;
+use std::io;
+use std::sync::Arc;
+use std::str::FromStr;
+use domain::base::Rtype;
+use domain::base::iana::Class;
+use domain::base::name::{Dname, FlattenInto};
+use domain::net::server::buf::VecBufSource;
+use domain::net::server::dgram::DgramServer;
+use domain::net::server::middleware::builder::MiddlewareBuilder;
+use domain::net::server::service::{CallResult, Transaction};
+use domain::net::server::stream::StreamServer;
+use domain::net::server::util::{mk_builder_for_target, mk_service, MkServiceRequest, MkServiceResult};
+use domain::zonefile::inplace::{Zonefile, Entry};
+use futures::future::pending;
+use tokio::net::{TcpListener, UdpSocket};
+use tokio::runtime::Runtime;
 use crate::config::{Config, ListenAddr};
 use crate::error::ExitError;
 use crate::process::Process;
 use crate::store::Store;
-use crate::zones::{Answer, SharedZoneSet, StoredDname, StoredRecord /*Zone*/};
-use bytes::Bytes;
-use domain::base::iana::Class;
-use domain::base::{Dname, Rtype, ToDname};
-use domain::net::server::buf::VecBufSource;
-use domain::net::server::dgram::DgramServer;
-use domain::net::server::middleware::builder::MiddlewareBuilder;
-use domain::net::server::prelude::*;
-use domain::net::server::service::{CallResult, Transaction};
-use domain::net::server::stream::StreamServer;
-use domain::rdata::{
-    Cname, Mb, Md, Mf, Minfo, Mr, Mx, Ns, Nsec, Ptr, Rrsig, Soa, Srv, ZoneRecordData,
-};
-use domain::zonefile::inplace::{Entry, Zonefile};
-use futures::future::pending;
-use std::fs::File;
-use std::io;
-use std::str::FromStr;
-use std::sync::Arc;
-use tokio::net::{TcpListener, UdpSocket};
-use tokio::runtime::Runtime;
+use crate::zones::{Answer, SharedZoneSet};
 
 pub fn prepare() -> Result<(), ExitError> {
     Process::init()?;
@@ -36,155 +34,59 @@ pub fn run(config: Config) -> Result<(), ExitError> {
     let process = Process::new(config);
     process.switch_logging(false)?;
 
-    Runtime::new()
-        .map_err(|_| ExitError::Generic)?
-        .block_on(async move {
-            let reader =
-                Zonefile::load(&mut File::open("/etc/nsd/example.com.zone").unwrap()).unwrap();
-            let mut zonefile = crate::zonefile::Zonefile::new(
-                StoredDname::from_str("example.com.").unwrap(),
-                Class::In,
-            );
-            for item in reader {
-                match item.unwrap() {
-                    Entry::Record(record) => {
-                        let (owner, class, ttl, data) =
-                            (record.owner(), record.class(), record.ttl(), record.data());
-                        let owner: Dname<Bytes> = owner.to_dname().unwrap();
-                        let data: ZoneRecordData<Bytes, StoredDname> = match data.to_owned() {
-                            ZoneRecordData::A(v) => v.into(),
-                            ZoneRecordData::Cname(v) => {
-                                ZoneRecordData::<Bytes, StoredDname>::Cname(Cname::new(
-                                    v.into_cname().to_canonical_dname().unwrap(),
-                                ))
-                            }
-                            ZoneRecordData::Hinfo(v) => v.into(),
-                            ZoneRecordData::Mb(v) => ZoneRecordData::<Bytes, StoredDname>::Mb(
-                                Mb::new(v.madname().to_dname().unwrap()),
-                            ),
-                            ZoneRecordData::Md(v) => ZoneRecordData::<Bytes, StoredDname>::Md(
-                                Md::new(v.madname().to_dname().unwrap()),
-                            ),
-                            ZoneRecordData::Mf(v) => ZoneRecordData::<Bytes, StoredDname>::Mf(
-                                Mf::new(v.madname().to_dname().unwrap()),
-                            ),
-                            ZoneRecordData::Minfo(v) => {
-                                ZoneRecordData::<Bytes, StoredDname>::Minfo(Minfo::new(
-                                    v.rmailbx().to_dname().unwrap(),
-                                    v.emailbx().to_dname().unwrap(),
-                                ))
-                            }
-                            ZoneRecordData::Mr(v) => ZoneRecordData::<Bytes, StoredDname>::Mr(
-                                Mr::new(v.newname().to_dname().unwrap()),
-                            ),
-                            ZoneRecordData::Mx(v) => ZoneRecordData::<Bytes, StoredDname>::Mx(
-                                Mx::new(v.preference(), v.exchange().to_dname().unwrap()),
-                            ),
-                            ZoneRecordData::Ns(v) => ZoneRecordData::<Bytes, StoredDname>::Ns(
-                                Ns::new(v.nsdname().to_dname().unwrap()),
-                            ),
-                            ZoneRecordData::Ptr(v) => ZoneRecordData::<Bytes, StoredDname>::Ptr(
-                                Ptr::new(v.ptrdname().to_dname().unwrap()),
-                            ),
-                            ZoneRecordData::Soa(v) => {
-                                ZoneRecordData::<Bytes, StoredDname>::Soa(Soa::new(
-                                    v.mname().to_dname().unwrap(),
-                                    v.rname().to_dname().unwrap(),
-                                    v.serial(),
-                                    v.refresh(),
-                                    v.retry(),
-                                    v.expire(),
-                                    v.minimum(),
-                                ))
-                            }
-                            ZoneRecordData::Txt(v) => v.into(),
-                            ZoneRecordData::Srv(v) => {
-                                ZoneRecordData::<Bytes, StoredDname>::Srv(Srv::new(
-                                    v.priority(),
-                                    v.weight(),
-                                    v.port(),
-                                    v.target().to_dname().unwrap(),
-                                ))
-                            }
-                            ZoneRecordData::Aaaa(v) => v.into(),
-                            ZoneRecordData::Dnskey(v) => v.into(),
-                            ZoneRecordData::Rrsig(v) => {
-                                ZoneRecordData::<Bytes, StoredDname>::Rrsig(
-                                    Rrsig::new(
-                                        v.type_covered(),
-                                        v.algorithm(),
-                                        v.labels(),
-                                        v.original_ttl(),
-                                        v.expiration(),
-                                        v.inception(),
-                                        v.key_tag(),
-                                        v.signer_name().to_dname().unwrap(),
-                                        v.signature().clone(),
-                                    )
-                                    .unwrap(),
-                                )
-                            }
-                            ZoneRecordData::Nsec(v) => ZoneRecordData::<Bytes, StoredDname>::Nsec(
-                                Nsec::new(v.next_name().to_dname().unwrap(), v.types().clone()),
-                            ),
-                            ZoneRecordData::Ds(v) => v.into(),
-                            ZoneRecordData::Dname(v) => {
-                                ZoneRecordData::<Bytes, StoredDname>::Dname(
-                                    domain::rdata::Dname::new(v.dname().clone().unwrap().1),
-                                )
-                            }
-                            ZoneRecordData::Nsec3(v) => v.into(),
-                            ZoneRecordData::Nsec3param(v) => v.into(),
-                            ZoneRecordData::Cdnskey(v) => v.into(),
-                            ZoneRecordData::Cds(v) => v.into(),
-                            ZoneRecordData::Unknown(v) => v.into(),
-                            _ => todo!(),
-                        };
-                        let record = StoredRecord::new(owner, class, ttl, data);
-                        zonefile.insert(record).unwrap();
-                    }
-                    _ => panic!("unsupported item"),
+    Runtime::new().map_err(|_| ExitError::Generic)?.block_on(async move {
+        let reader = Zonefile::load(&mut File::open("/etc/nsd/example.com.zone").unwrap()).unwrap();
+        let mut zonefile = crate::zonefile::Zonefile::new(
+            Dname::from_str("example.com.").unwrap(),
+            Class::In
+        );
+        for item in reader {
+            match item.unwrap() {
+                Entry::Record(record) => {
+                    zonefile.insert(record.flatten_into()).unwrap();
                 }
+                _ => panic!("unsupported item")
             }
-            let zone = zonefile.into_zone_builder().unwrap().finalize();
+        }
+        let zone = zonefile.into_zone_builder().unwrap().finalize();
 
-            let store = match Store::init(process.config().data_dir.clone()) {
-                Ok(store) => store,
+        let store = match Store::init(process.config().data_dir.clone()) {
+            Ok(store) => store,
+            Err(err) => {
+                eprintln!("Failed to open database: {}", err);
+                return Err(ExitError::Generic);
+            }
+        };
+        let zones = match process.config().initialize {
+            true => match SharedZoneSet::init(store).await {
+                Ok(zones) => zones,
                 Err(err) => {
-                    eprintln!("Failed to open database: {}", err);
+                    eprintln!("Failed to initialize zones: {}", err);
                     return Err(ExitError::Generic);
                 }
-            };
-            let zones = match process.config().initialize {
-                true => match SharedZoneSet::init(store).await {
-                    Ok(zones) => zones,
-                    Err(err) => {
-                        eprintln!("Failed to initialize zones: {}", err);
-                        return Err(ExitError::Generic);
-                    }
-                },
-                false => match SharedZoneSet::load(store).await {
-                    Ok(zones) => zones,
-                    Err(err) => {
-                        eprintln!("Failed to load zones: {}", err);
-                        return Err(ExitError::Generic);
-                    }
-                },
-            };
+            },
+            false => match SharedZoneSet::load(store).await {
+                Ok(zones) => zones,
+                Err(err) => {
+                    eprintln!("Failed to load zones: {}", err);
+                    return Err(ExitError::Generic);
+                }
+            },
+        };
 
-            zones.write().await.insert_zone(zone).await.unwrap();
+        zones.write().await.insert_zone(zone).await.unwrap();
 
-            for addr in process.config().listen.iter().cloned() {
-                eprintln!("Binding on {}", addr);
-                let zones = zones.clone();
-                tokio::spawn(async move {
-                    if let Err(err) = server(addr, zones).await {
-                        println!("{}", err);
-                    }
-                });
-            }
-            pending().await
-        })
+        for addr in process.config().listen.iter().cloned() {
+            eprintln!("Binding on {:?}", addr);
+            let zones = zones.clone();
+            tokio::spawn(async move {
+                if let Err(err) = server(addr, zones).await {
+                    println!("{}", err);
+                }
+            });
+        }
+        pending().await
+    })
 }
 
 #[cfg(feature = "tls")]
@@ -226,9 +128,9 @@ mod tls {
 }
 
 async fn server(addr: ListenAddr, zones: SharedZoneSet) -> Result<(), io::Error> {
-    let buf = Arc::new(VecBufSource);
+    let buf = VecBufSource;
     let middleware = MiddlewareBuilder::<Vec<u8>>::default().finish();
-    let svc = mk_service(query::<Vec<u8>>, zones).into();
+    let svc = mk_service(query::<Vec<u8>>, zones);
     match addr {
         ListenAddr::Udp(addr) => {
             let sock = UdpSocket::bind(addr).await?;
