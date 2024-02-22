e6a17fab8f67c52ba542669f52329b20f6abb299
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-02-22 13:42:26 +0100

    Update to the new transaction stream domain API and use the new zonetree walking support to more fully implement AXFR.

--- src/operation.rs
+++ src/operation.rs
@@ -3,24 +3,27 @@
 use crate::config::{Config, ListenAddr};
 use crate::error::ExitError;
 use crate::process::Process;
-use domain::base::iana::Class;
+use domain::base::iana::{Class, Opcode};
+use domain::base::message_builder::AdditionalBuilder;
 use domain::base::name::{Dname, FlattenInto};
 use domain::base::Rtype;
+use domain::dep::octseq::OctetsBuilder;
 use domain::net::server::buf::VecBufSource;
 use domain::net::server::dgram::DgramServer;
 use domain::net::server::middleware::builder::MiddlewareBuilder;
-use domain::net::server::service::{CallResult, Transaction};
+use domain::net::server::service::{CallResult, Transaction, TransactionStream};
 use domain::net::server::stream::StreamServer;
 use domain::net::server::util::{
     mk_builder_for_target, mk_service, MkServiceRequest, MkServiceResult,
 };
 use domain::zonefile::inplace::{Entry, Zonefile};
-use domain::zonetree::{Answer, ZoneSet};
+use domain::zonetree::{Answer, AnswerContent, ZoneSet};
 use futures::future::pending;
 use std::fs::File;
+use std::future::ready;
 use std::io;
 use std::str::FromStr;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, UdpSocket};
 use tokio::runtime::Runtime;
 
@@ -145,44 +148,149 @@ async fn server(addr: ListenAddr, zones: Arc<ZoneSet>) -> Result<(), io::Error> 
 
 type MyError = ();
 
+fn set_axfr_header<Target>(
+    msg: &MkServiceRequest<Vec<u8>>,
+    additional: &mut AdditionalBuilder<Target>,
+) where
+    Target: AsMut<[u8]>,
+    Target: OctetsBuilder,
+{
+    // https://datatracker.ietf.org/doc/html/rfc5936#section-2.2.1
+    // 2.2.1: Header Values
+    //
+    // "These are the DNS message header values for AXFR responses.
+    //
+    //     ID          MUST be copied from request -- see Note a)
+    //
+    //     QR          MUST be 1 (Response)
+    //
+    //     OPCODE      MUST be 0 (Standard Query)
+    //
+    //     Flags:
+    //        AA       normally 1 -- see Note b)
+    //        TC       MUST be 0 (Not truncated)
+    //        RD       RECOMMENDED: copy request's value; MAY be set to 0
+    //        RA       SHOULD be 0 -- see Note c)
+    //        Z        "mbz" -- see Note d)
+    //        AD       "mbz" -- see Note d)
+    //        CD       "mbz" -- see Note d)"
+    let header = additional.header_mut();
+    header.set_id(msg.header().id());
+    header.set_qr(true);
+    header.set_opcode(Opcode::Query);
+    header.set_aa(true);
+    header.set_tc(false);
+    header.set_rd(msg.header().rd());
+    header.set_ra(false);
+    header.set_z(false);
+    header.set_ad(false);
+    header.set_cd(false);
+}
+
 fn query<Target>(
     msg: MkServiceRequest<Vec<u8>>,
     zones: Arc<ZoneSet>,
 ) -> MkServiceResult<Vec<u8>, MyError> {
     let qtype = msg.sole_question().unwrap().qtype();
     if qtype == Rtype::Axfr {
-        let mut txn = Transaction::stream();
-
-        let cloned_msg = msg.clone();
-        let cloned_zones = zones.clone();
-        txn.push(async move {
-            let question = cloned_msg.sole_question().unwrap();
-            let answer = cloned_zones
-                .find_zone(question.qname(), question.qclass())
-                .map(|zone| zone.read(None))
-                .and_then(|zone| zone.query(question.qname(), Rtype::Soa).ok())
-                .unwrap_or_else(|| Answer::refused());
-            let builder = mk_builder_for_target();
-            let additional = answer.to_message(&cloned_msg, builder);
-            Ok(CallResult::new(additional))
-        });
-
-        let cloned_msg = msg.clone();
-        let cloned_zones = zones.clone();
-        txn.push(async move {
-            let question = cloned_msg.sole_question().unwrap();
-            let zone = cloned_zones
+        let stream_fut = async move {
+            let question = msg.sole_question().unwrap();
+            let zone = zones
                 .find_zone(question.qname(), question.qclass())
                 .map(|zone| zone.read(None));
-            let answer = match zone {
-                Some(zone) => zone.query(question.qname(), Rtype::Soa).unwrap(),
-                None => Answer::refused(),
-            };
-            let builder = mk_builder_for_target();
-            let additional = answer.to_message(&cloned_msg, builder);
-            Ok(CallResult::new(additional))
-        });
-        Ok(txn)
+
+            let stream = Arc::new(Mutex::new(TransactionStream::default()));
+
+            if let Some(zone) = zone {
+                // https://datatracker.ietf.org/doc/html/rfc5936#section-2.2
+                // 2.2: AXFR Response
+                //
+                // "An AXFR response that is transferring the zone's contents
+                //  will consist of a series (which could be a series of
+                //  length 1) of DNS messages.  In such a series, the first
+                //  message MUST begin with the SOA resource record of the
+                //  zone, and the last message MUST conclude with the same SOA
+                //  resource record.  Intermediate messages MUST NOT contain
+                //  the SOA resource record.  The AXFR server MUST copy the
+                //  Question section from the corresponding AXFR query message
+                //  into the first response message's Question section.  For
+                //  subsequent messages, it MAY do the same or leave the
+                //  Question section empty."
+
+                // Get the SOA record as AXFR transfers must start and end
+                // with the SOA record
+                let question = msg.sole_question().unwrap();
+                let answer = zone
+                    .query(question.qname(), Rtype::Soa)
+                    .unwrap_or_else(|_| Answer::refused());
+                let builder = mk_builder_for_target();
+                let mut additional = answer.to_message(&msg, builder);
+                set_axfr_header(&msg, &mut additional);
+                let begin_soa_response = CallResult::new(additional);
+                let end_soa_response = begin_soa_response.clone();
+
+                // Note: The code above already added the question from the
+                // request into the SOA response messages.
+
+                // Push the begin SOA response message into the stream
+                stream.lock().unwrap().push(ready(Ok(begin_soa_response)));
+
+                // "The AXFR protocol treats the zone contents as an unordered
+                //  collection (or to use the mathematical term, a "set") of
+                //  RRs.  Except for the requirement that the transfer must
+                //  begin and end with the SOA RR, there is no requirement to
+                //  send the RRs in any particular order or grouped into
+                //  response messages in any particular way.  Although servers
+                //  typically do attempt to send related RRs (such as the RRs
+                //  forming an RRset, and the RRsets of a name) as a
+                //  contiguous group or, when message space allows, in the
+                //  same response message, they are not required to do so, and
+                //  clients MUST accept any ordering and grouping of the
+                //  non-SOA RRs.  Each RR SHOULD be transmitted only once, and
+                //  AXFR clients MUST ignore any duplicate RRs received.
+                //
+                //  Each AXFR response message SHOULD contain a sufficient
+                //  number of RRs to reasonably amortize the per-message
+                //  overhead, up to the largest number that will fit within a
+                //  DNS message (taking the required content of the other
+                //  sections into account, as described below).
+                //
+                //  Some old AXFR clients expect each response message to
+                //  contain only a single RR.  To interoperate with such
+                //  clients, the server MAY restrict response messages to a
+                //  single RR.  As there is no standard way to automatically
+                //  detect such clients, this typically requires manual
+                //  configuration at the server."
+
+                // TODO: Support a setting for sending only one RR per
+                // response message for backward compatibility.
+
+                let cloned_msg = msg.clone();
+                let cloned_stream = stream.clone();
+                zone.walk(Box::new(move |answer: Answer| {
+                    if let AnswerContent::Data(rrset) = answer.content() {
+                        if rrset.rtype() != Rtype::Soa {
+                            let builder = mk_builder_for_target();
+                            let mut additional = answer.to_message(&cloned_msg, builder);
+                            set_axfr_header(&cloned_msg, &mut additional);
+                            cloned_stream
+                                .lock()
+                                .unwrap()
+                                .push(ready(Ok(CallResult::new(additional))));
+                        }
+                    }
+                }));
+
+                // Push the end SOA response message into the stream
+                stream.lock().unwrap().push(ready(Ok(end_soa_response)));
+            }
+
+            let mutex = Arc::into_inner(stream).unwrap();
+            let stream = mutex.into_inner().unwrap();
+            stream
+        };
+
+        Ok(Transaction::stream(Box::pin(stream_fut)))
     } else {
         let fut = async move {
             let question = msg.sole_question().unwrap();
