ae06e62e74066e8371f30c72ade1a1cce646ac66
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-03-14 10:05:59 +0100

    Minor improvement to signed zone hook script.

--- approve_or_deny_signed.sh
+++ approve_or_deny_signed.sh
@@ -8,7 +8,7 @@ ZONE_NAME="$1"
 ZONE_SERIAL="$2"
 APPROVAL_TOKEN="$3"
 
-dig +noall +answer @127.0.0.1 -p 8057 example.com AXFR | dnssec-verify -o example.com /dev/stdin /tmp/keys/ || {
+dig +noall +onesoa +answer @127.0.0.1 -p 8057 example.com AXFR | dnssec-verify -o example.com /dev/stdin /tmp/keys/ || {
     wget -qO- http://127.0.0.1:8080/rs2/${ZONE_NAME}/${ZONE_SERIAL}/reject/${APPROVAL_TOKEN}
     exit 0
 }
