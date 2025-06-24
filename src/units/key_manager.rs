35dad489e3e4a39b799d779da7809bc1a14f1e5c
Philip Homburg <philip@nlnetlabs.nl>
2025-06-24 12:36:24 +0200

    React to changes in the state file and implement cron_next.

--- src/units/key_manager.rs
+++ src/units/key_manager.rs
@@ -6,13 +6,17 @@ use crate::metrics;
 use crate::payload::Update;
 use core::fmt::Display;
 use core::time::Duration;
+use domain::dnssec::sign::keys::keyset::{KeySet, UnixTime};
 use domain::zonetree::Zone;
 use log::{error, info};
 use serde::Deserialize;
 use serde_with::serde_as;
+use std::collections::HashMap;
+use std::fs::metadata;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 use tokio::time::MissedTickBehavior;
 
 #[serde_as]
@@ -66,6 +70,7 @@ struct KeyManager {
     gate: Gate,
     metrics: Arc<KeyManagerMetrics>,
     status_reporter: Arc<KeyManagerStatusReporter>,
+    ks_info: Mutex<HashMap<String, KeySetInfo>>,
 }
 
 impl KeyManager {
@@ -81,6 +86,7 @@ impl KeyManager {
             gate,
             metrics,
             status_reporter,
+            ks_info: Mutex::new(HashMap::new()),
         }
     }
 
@@ -97,7 +103,7 @@ impl KeyManager {
         loop {
             tokio::select! {
                 _ = interval.tick() => {
-                    eprintln!("TICK");
+                    //eprintln!("TICK");
                     self.tick(dnst_keyset_bin_path.as_path(), dnst_keyset_data_dir.as_path()).await;
                 }
 
@@ -146,39 +152,132 @@ impl KeyManager {
 
     async fn tick(&self, dnst_keyset_bin_path: &Path, dnst_keyset_data_dir: &Path) {
         let zone_tree = self.component.unsigned_zones();
+        let mut ks_info = self.ks_info.lock().await;
         for zone in zone_tree.load().iter_zones() {
             let apex_name = zone.apex_name().to_string();
-            let cfg_path = dnst_keyset_data_dir.join(format!("{apex_name}.cfg"));
-            let mut args = vec!["keyset", "-c"];
-            args.push(cfg_path.to_str().unwrap());
-            args.push("cron");
-            println!(
-                "Invoking keyset cron for zone {apex_name} with {}",
-                args.join(" ")
-            );
-            let Ok(res) = Command::new(dnst_keyset_bin_path).args(args).output() else {
-                error!(
-                    "Failed to invoke keyset binary at '{}",
-                    dnst_keyset_bin_path.display()
-                );
+            let state_path = Path::new("/tmp/").join(format!("{apex_name}.state"));
+            let info = ks_info.get(&apex_name);
+            let Some(info) = info else {
+                let value = get_keyset_info(state_path);
+                let _ = ks_info.insert(apex_name, value);
                 continue;
             };
-            if res.status.success() {
-                println!("CRON OUT: {}", String::from_utf8_lossy(&res.stdout));
-                // TOOD: Check the cron output and decide if a resign event should be sent.
-                // if ... {
-                //     self.gate
-                //         .update_data(Update::ResignZoneEvent {
-                //             zone_name: zone.apex_name().clone(),
-                //         })
-                //         .await;
-                // }
-            } else {
-                println!("CRON ERR: {}", String::from_utf8_lossy(&res.stderr));
+
+            let keyset_state_modified = file_modified(&state_path).unwrap();
+            if keyset_state_modified != info.keyset_state_modified {
+                // Keyset state file is modified. Update our data and
+                // signal the signer to re-sign the zone.
+                let new_info = get_keyset_info(&state_path);
+                let _ = ks_info.insert(apex_name, new_info);
+                self.gate
+                    .update_data(Update::ResignZoneEvent {
+                        zone_name: zone.apex_name().clone(),
+                    })
+                    .await;
+                continue;
+            }
+
+            let Some(ref cron_next) = info.cron_next else {
+                continue;
+            };
+
+            if *cron_next < UnixTime::now() {
+                // Run cron
+                let cfg_path = dnst_keyset_data_dir.join(format!("{apex_name}.cfg"));
+                let mut args = vec!["keyset", "-c"];
+                args.push(cfg_path.to_str().unwrap());
+                args.push("cron");
+                println!(
+                    "Invoking keyset cron for zone {apex_name} with {}",
+                    args.join(" ")
+                );
+                let Ok(res) = Command::new(dnst_keyset_bin_path).args(args).output() else {
+                    error!(
+                        "Failed to invoke keyset binary at '{}",
+                        dnst_keyset_bin_path.display()
+                    );
+
+                    // Clear cron_next.
+                    let info = KeySetInfo {
+                        cron_next: None,
+                        keyset_state_modified: info.keyset_state_modified.clone(),
+                        retries: 0,
+                    };
+                    let _ = ks_info.insert(apex_name, info);
+                    continue;
+                };
+
+                if res.status.success() {
+                    println!("CRON OUT: {}", String::from_utf8_lossy(&res.stdout));
+
+                    // We expect cron to change the state file. If
+                    // that is the case, get a new KeySetInfo and notify
+                    // the signer.
+                    let new_info = get_keyset_info(&state_path);
+                    if new_info.keyset_state_modified != info.keyset_state_modified {
+                        // Something happened. Update ks_info and signal the
+                        // signer.
+                        let new_info = get_keyset_info(&state_path);
+                        let _ = ks_info.insert(apex_name, new_info);
+                        self.gate
+                            .update_data(Update::ResignZoneEvent {
+                                zone_name: zone.apex_name().clone(),
+                            })
+                            .await;
+                        continue;
+                    }
+
+                    // Nothing happened. Assume that the timing could be off.
+                    // Try again in a minute. After a few tries log an error
+                    // and give up.
+                    let cron_next = cron_next.clone() + Duration::from_secs(60);
+                    let new_info = KeySetInfo {
+                        cron_next: Some(cron_next),
+                        keyset_state_modified: info.keyset_state_modified.clone(),
+                        retries: info.retries + 1,
+                    };
+                    if new_info.retries >= CRON_MAX_RETRIES {
+                        error!(
+			    "The command 'dnst keyset cron' for config {} failed to update state file {}", cfg_path.display(), state_path.display()
+			);
+
+                        // Clear cron_next.
+                        let info = KeySetInfo {
+                            cron_next: None,
+                            keyset_state_modified: info.keyset_state_modified.clone(),
+                            retries: 0,
+                        };
+                        let _ = ks_info.insert(apex_name, info);
+                        continue;
+                    }
+                    let _ = ks_info.insert(apex_name, new_info);
+                    continue;
+                } else {
+                    println!("CRON ERR: {}", String::from_utf8_lossy(&res.stderr));
+                    // Clear cron_next.
+                    let info = KeySetInfo {
+                        cron_next: None,
+                        keyset_state_modified: info.keyset_state_modified.clone(),
+                        retries: 0,
+                    };
+                    let _ = ks_info.insert(apex_name, info);
+                }
             }
         }
     }
 }
+
+//------------ KeySetInfo ----------------------------------------------------
+#[derive(Clone)]
+pub struct KeySetInfo {
+    keyset_state_modified: UnixTime,
+    cron_next: Option<UnixTime>,
+    retries: u32,
+}
+
+// Maximum number of times to try the cron command when the state file does
+// not change.
+const CRON_MAX_RETRIES: u32 = 5;
 
 //------------ KeyManagerMetrics ---------------------------------------------
 
@@ -266,5 +365,43 @@ impl Chainable for KeyManagerStatusReporter {
 impl Named for KeyManagerStatusReporter {
     fn name(&self) -> &str {
         &self.name
+    }
+}
+
+fn file_modified(filename: impl AsRef<Path>) -> Result<UnixTime, String> {
+    let md = metadata(filename).unwrap();
+    let modified = md.modified().unwrap();
+    modified
+        .try_into()
+        .map_err(|e| format!("unable to convert from SystemTime: {e}"))
+}
+
+fn get_keyset_info(state_path: impl AsRef<Path>) -> KeySetInfo {
+    // Get the modified time of the state file before we read
+    // state file itself. This is safe if there is a concurrent
+    // update.
+    let keyset_state_modified = file_modified(&state_path).unwrap();
+
+    /// Persistent state for the keyset command.
+    /// Copied frmo the keyset branch of dnst.
+    #[derive(Deserialize)]
+    struct KeySetState {
+        /// Domain KeySet state.
+        keyset: KeySet,
+
+        dnskey_rrset: Vec<String>,
+        ds_rrset: Vec<String>,
+        cds_rrset: Vec<String>,
+        ns_rrset: Vec<String>,
+        cron_next: Option<UnixTime>,
+    }
+
+    let state = std::fs::read_to_string(state_path).unwrap();
+    let state: KeySetState = serde_json::from_str(&state).unwrap();
+
+    KeySetInfo {
+        keyset_state_modified,
+        cron_next: state.cron_next,
+        retries: 0,
     }
 }
