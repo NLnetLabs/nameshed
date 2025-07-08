cc3bb09f70463060f3a82e350b2533896faa36c1
arya dradjica <arya@nlnetlabs.nl>
2025-07-08 11:39:39 +0200

    [path_monitor] Integrate 'notify'

--- src/path_monitor.rs
+++ src/path_monitor.rs
@@ -33,52 +33,115 @@
 //! - A directory along the path (or the path it points to, if it is a symlink)
 //!   is mounted or unmounted.
 
-use std::{marker::PhantomData, path::Path};
+use std::{
+    collections::hash_map,
+    path::Path,
+    sync::{Arc, Mutex, Weak},
+};
+
+use notify::{RecursiveMode, Watcher};
 
 //----------- PathMonitor ------------------------------------------------------
 
 /// A monitor for changes to paths.
 pub struct PathMonitor<CH: PathChangeHandler> {
-    /// The associated handlers.
-    _handlers: PhantomData<Vec<CH>>,
+    /// The underlying monitor.
+    inner: Arc<Mutex<Inner<CH>>>,
+}
+
+struct Inner<CH> {
+    /// The underlying watcher.
+    watcher: notify::RecommendedWatcher,
+
+    /// A map of all paths to handlers.
+    handlers: foldhash::HashMap<Box<Path>, Vec<Arc<CH>>>,
 }
 
 impl<CH: PathChangeHandler> PathMonitor<CH> {
     /// Set up a [`PathMonitor`].
-    pub fn new() -> (Self, PathMonitorRunner<CH>) {
-        (
-            Self {
-                _handlers: PhantomData,
-            },
-            PathMonitorRunner {
-                _public: PhantomData,
-            },
-        )
+    #[allow(clippy::new_without_default)] // will return Result later
+    pub fn new() -> Self {
+        let inner = Arc::new_cyclic(|inner: &Weak<Mutex<Inner<CH>>>| {
+            let handler_inner = inner.clone();
+            let handler = move |event: notify::Result<notify::Event>| {
+                let Some(inner) = handler_inner.upgrade() else {
+                    return;
+                };
+
+                let Ok(lock) = inner.lock() else {
+                    return;
+                };
+
+                let paths = match event {
+                    Ok(event) => event.paths,
+                    Err(error) => error.paths,
+                };
+
+                for path in paths {
+                    let Some(handlers) = lock.handlers.get(&*path) else {
+                        continue;
+                    };
+
+                    for handler in handlers {
+                        handler.react();
+                    }
+                }
+            };
+
+            let watcher = notify::recommended_watcher(handler).unwrap();
+            let handlers = Default::default();
+            Mutex::new(Inner { watcher, handlers })
+        });
+
+        Self {
+            inner: inner.clone(),
+        }
     }
 
     /// Watch a path.
-    pub fn add(&self, handler: CH) {
-        let _ = handler;
+    pub fn add(&self, handler: CH) -> Arc<CH> {
+        let handler = Arc::new(handler);
+        let path = handler.path();
+        let mut lock = self.inner.lock().unwrap();
+        let inner = &mut *lock;
+
+        let handlers = inner.handlers.entry(path.into()).or_insert(Vec::new());
+        if handlers.is_empty() {
+            inner
+                .watcher
+                .watch(path, RecursiveMode::NonRecursive)
+                .unwrap();
+        }
+
+        handlers.push(handler.clone());
+
+        handler
     }
-}
 
-//----------- PathMonitorRunner ------------------------------------------------
+    /// Stop watching a path.
+    pub fn remove(&self, handler: Arc<CH>) {
+        let path = handler.path();
+        let mut lock = self.inner.lock().unwrap();
 
-/// A runner driving a [`PathMonitor`].
-pub struct PathMonitorRunner<CH: PathChangeHandler> {
-    /// The associated public state.
-    _public: PhantomData<PathMonitor<CH>>,
-}
+        let mut handlers = match lock.handlers.entry(path.into()) {
+            hash_map::Entry::Occupied(entry) => entry,
+            hash_map::Entry::Vacant(_) => panic!("handler not registered"),
+        };
 
-impl<CH: PathChangeHandler> PathMonitorRunner<CH> {
-    /// Drive the associated [`PathMonitor`], non-blockingly.
-    pub fn poll(&mut self) {}
+        // Remove this handler from the list.
+        handlers.get_mut().retain(|h| !Arc::ptr_eq(h, &handler));
+
+        if handlers.get_mut().is_empty() {
+            handlers.remove();
+            lock.watcher.unwatch(path);
+        }
+    }
 }
 
 //----------- PathChangeHandler ------------------------------------------------
 
 /// A watcher for changes to a path.
-pub trait PathChangeHandler {
+pub trait PathChangeHandler: Send + Sync + 'static {
     /// The path to watch.
     fn path(&self) -> &Path;
 
