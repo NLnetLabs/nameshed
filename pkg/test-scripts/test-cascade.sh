bcacf165b2e2f7804998c13d9779922540f609aa
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-09-02 14:45:41 +0200

    Various fixes.
    - Remove outdated TODO comments.
    - Fix references to cascade.conf instead of config.toml.
    - Fix references to cascade instead of cascaded.
    - Try going back to pkg/common/cascaded.service instead of
    cascaded.cascaded.service now that the other types are fixed.

--- pkg/test-scripts/test-cascade.sh
+++ pkg/test-scripts/test-cascade.sh
@@ -5,31 +5,23 @@ set -x
 
 case $1 in
   post-install)
-    #echo -e "\nCASCADE VERSION:"
-    #VER=$(cascade --version)
-    #echo $VER
+  post-upgrade)
+    echo -e "\nCASCADE VERSION:"
+    cascade --version
 
-    echo -e "\nCASCADE CONF:"
-    cat /etc/cascade/cascade.conf
+    echo -e "\nCASCADED VERSION:"
+    cascaded --version
 
-    echo -e "\nCASCADE SERVICE STATUS:"
-    systemctl status cascade || true
+    echo -e "\nCASCADED CONF:"
+    cat /etc/cascade/cascaded.conf
+
+    echo -e "\nCASCADED SERVICE STATUS:"
+    systemctl status cascaded || true
 
     #echo -e "\nCASCADE MAN PAGE (first 20 lines only):"
     #man -P cat cascade | head -n 20 || true
-    ;;
 
-  post-upgrade)
-    #echo -e "\nCASCADE VERSION:"
-    #cascade --version
-    
-    echo -e "\nCASCADE CONF:"
-    cat /etc/cascade/cascade.conf
-    
-    echo -e "\nCASCADE SERVICE STATUS:"
-    systemctl status cascade || true
-    
-    #echo -e "\nCASCADE MAN PAGE:"
-    #man -P cat cascade
+    #echo -e "\nCASCADED MAN PAGE (first 20 lines only):"
+    #man -P cat cascaded | head -n 20 || true
     ;;
 esac
