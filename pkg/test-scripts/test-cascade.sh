d270d8046400cbddba8ce1d4bf43c688376c2585
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-09-04 12:08:37 +0200

    Packaging fixes. (#42)
    - FIX: Asset file not found: target/rpm/cascaded.service
    - Remove outdated TODO comments.
    - Fix references to cascade.conf instead of config.toml.
    - Fix references to cascade instead of cascaded.
    * Reformat asset lines to be readable.
    * Make rpmlint happy.
    * Use dnst and kmip2pkcs11 from packages.nlnetlabs.nl proposed channel as both are now published there, if not yet the right version of dnst.
    * Add a comment about the use of a custom repository.
    * Use the new Ploutos v9 release.
    * Add more O/S variants to package.

--- pkg/test-scripts/test-cascade.sh
+++ pkg/test-scripts/test-cascade.sh
@@ -4,32 +4,26 @@ set -eo pipefail
 set -x
 
 case $1 in
-  post-install)
-    #echo -e "\nCASCADE VERSION:"
-    #VER=$(cascade --version)
-    #echo $VER
+  post-install|post-upgrade)
+    echo -e "\nCASCADE VERSION:"
+    cascade --version
 
-    echo -e "\nCASCADE CONF:"
-    cat /etc/cascade/cascade.conf
+    echo -e "\nCASCADED VERSION:"
+    cascaded --version
 
-    echo -e "\nCASCADE SERVICE STATUS:"
-    systemctl status cascade || true
+    echo -e "\nDNST VERSION:"
+    dnst --version
+
+    echo -e "\nCASCADED CONF:"
+    cat /etc/cascade/config.toml
+
+    echo -e "\nCASCADED SERVICE STATUS:"
+    systemctl status cascaded || true
 
     #echo -e "\nCASCADE MAN PAGE (first 20 lines only):"
     #man -P cat cascade | head -n 20 || true
-    ;;
 
-  post-upgrade)
-    #echo -e "\nCASCADE VERSION:"
-    #cascade --version
-    
-    echo -e "\nCASCADE CONF:"
-    cat /etc/cascade/cascade.conf
-    
-    echo -e "\nCASCADE SERVICE STATUS:"
-    systemctl status cascade || true
-    
-    #echo -e "\nCASCADE MAN PAGE:"
-    #man -P cat cascade
+    #echo -e "\nCASCADED MAN PAGE (first 20 lines only):"
+    #man -P cat cascaded | head -n 20 || true
     ;;
 esac
